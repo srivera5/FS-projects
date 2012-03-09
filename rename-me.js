3b331c049d525c097023f2a653d809260e2dfb59
srivera5 <srivera5@fullsail.edu>
2012-03-08 22:44:42 -0500

    Added Procedure

--- rename-me.js
+++ rename-me.js
@@ -1,6 +1,15 @@
+var getEggs = function (eggs) {
+    if (eggs > 2) {
+		console.log ("It's french toast time!");
+	} else {
+		console.log ("WTH!");
+	};
+};
+	getEggs(3);
+	
 var morningGreeting = function (name, timeOfDay) {
     var greeting = timeOfDay + "," + name + "! ";
     return greeting;
 };
 var fullGreeting = morningGreeting("Leo", "Good morning");
-   console.log(fullGreeting + "Brekfast is served.");
+ 	console.log(fullGreeting + "Breakfast is served.");
