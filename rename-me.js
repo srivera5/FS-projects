94c5addd144e4d171248468ec6bfc81c3998add2
srivera5 <srivera5@fullsail.edu>
2012-03-08 22:21:40 -0500

    added String Function

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,6 @@
-alert("JavaScript works!");
+var morningGreeting = function (name, timeOfDay) {
+    var greeting = timeOfDay + "," + name + "! ";
+    return greeting;
+};
+var fullGreeting = morningGreeting("Leo", "Good morning");
+   console.log(fullGreeting + "Brekfast is served.");
