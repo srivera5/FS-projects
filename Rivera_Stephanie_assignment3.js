e9fd382afcaf0aa04c46155bffef41bd45627e72
Generic <generic@new-host.home>
2012-04-19 19:09:32 -0400

    added new method procedure to storyline

--- Rivera_Stephanie_assignment3.js
+++ Rivera_Stephanie_assignment3.js
@@ -2,7 +2,7 @@
 //Story plot: shopping adventure
 //SDI Project 3
 
-//Object with Accessor
+//Object with Accessor: Story begins with deciding what to do
 var myself = {
 	hobby: "shopping", //Property: String
 	bored: true, //Property: Boolean
@@ -18,9 +18,41 @@ myself.sayHobby();
 myself.hobby ="shopping with a friend.";
 myself.sayHobby();
 
+//Method Procedure :Look through address book and find friend to go with
+var friend1 = {
+	name: "Bekah", //Property String
+	number: "258-6767",
+	avail: "not available to go with me to the mall.",
+	answer: function() {
+	console.log(this.name + " is " + this.avail); } //Procedure Method
+	}
+var friend2 = {
+	name: "Crystal", //Property String
+	number: "258-6798",
+	avail: "not available to go with me to the mall.",
+	answer: function() {
+	console.log(this.name + " is " + this.avail); }  //Procedure Method
+	}
+var friend3 = {
+	name: "Londy", //Property Strings
+	number: "258-6701",
+	avail: "available to go with me to the mall.",
+	answer: function() {
+	console.log(this.name + " is " + this.avail); }  //Procedure Method
+	}
 
+var friends = [friend1, friend2, friend3];
 
+var call = function(invite) {
+	return invite.answer(); }
 
+var listOfFriends = function() {
+	var i = friends.length;
+	for(i=0; i<friends.length; i++) {
+	call(friends[i]); }
+};
+listOfFriends();
+	
 //First shop at the mall. Decide if I like a dress.
 
 var outfitWorthy = function (brandName,color) {
