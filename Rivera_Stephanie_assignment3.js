460aee8a33b88bf85c142ef7bb1fa971cd8f08fa
Generic <generic@new-host.home>
2012-04-19 22:24:26 -0400

    added another function using arrays

--- Rivera_Stephanie_assignment3.js
+++ Rivera_Stephanie_assignment3.js
@@ -2,6 +2,8 @@
 //Story plot: shopping adventure
 //SDI Project 3
 
+//Global variables
+
 //Object with Accessor: Story begins with deciding what to do
 var myself = {
 	hobby: "shopping", //Property: String
@@ -14,11 +16,14 @@ var myself = {
 		} else if (numTvs>=1) { //Nested Conditional
 		console.log("I'll just watch some more " + this.shows[2] + ".");
 		} else { 
-		console.log("I guess I'll go study.");
+		console.log("I guess I'll go study."); }
 	}
 }
 myself.sayHobby();
-myself.hobby ="shopping with a friend.";
+myself.sayHobby = function() {
+		console.log("I'd like to go shopping with a friend.");//Mutator Function
+	}
+//myself.hobby ="shopping with a friend.";
 myself.sayHobby();
 
 //Method Procedure :Look through address book and find friend to go with
@@ -44,10 +49,10 @@ var friend3 = {
 	console.log(this.name + " is " + this.avail); }  //Procedure Method
 	}
 
-var friends = [friend1, friend2, friend3];
+var friends = [friend1, friend2, friend3]; //Local Variable
 
 function call(invite) { //Function
-	return invite.answer(); }
+	return invite.answer(); } //return array listed under call(friends[i])
 
 function listOfFriends() { 
 	var i = friends.length;
@@ -55,44 +60,51 @@ function listOfFriends() {
 	call(friends[i]); }
 };
 listOfFriends();
-	
-//First shop at the mall. Decide if I like a dress.
 
-var outfitWorthy = function (brandName,color) {
+//Figure out my spending
+
+var paycheck = {
+	bills: 200,
+	check: 1000,
+	calcSpend: function () {
+ 		var p = this.check - (this.bill * 2);
+ 		return p;
+		console.log("I have " + p + "spend!"); } }
+paycheck.calcSpend(this.check);
+
+	
+//First shop at the mall. Decide if I have enough money to purchase.
+
+var shopping = {
+	costHandbag: 60,
+	costShoes: 100,
+	money: 200,
+	handbag: function (brandName,color) {
   
     if (brandName === "Betsey Johnson") {
-      if (color === 'skullblack' || color === 'cottoncandyblue') {
+      if ((color === 'skullblack' || color === 'cottoncandyblue') && 
+      (this.money > this.costShoes + this.costHandbag)) {
             console.log("OMFG! Take my credit card now!");
       } else {
 		console.log("Meh. I can do better.");
       }
     } else {
       return false;
-    }
-};
-outfitWorthy("Betsey Johnson", "hotpink");
+    } }
+}
+shopping.handbag("Betsey Johnson", "cottoncandyblue");
 
-//shopping spree/allowance
+//Other stores I'd like to look at
 
-var paycheck = function (x) {
-  return x * x;
-};
+var mallStore = function (storeName, open) {
+	this.storeName = storeName;
+	this.open = open;
+	}
+var mall = new Array();
+	mall[0] = new mallStore ("Dilliards", true);
+	mall[1] = new mallStore ("H&N", false);
+	mall[2] = new mallStore ("Forever 21", true);
+var i = 0;
+while (i<mall.length) {
+	console.log(mall[i].storeName); i++;}
 
-var irsRefund = function (x) {
-  return paycheck(x) * x;
-  console.log("I have " + x + "spend!");
-};
-
-paycheck(1000);
-
-//NOTE! Revise paycheck function to include math on bills as such below:
-var budget = function (bills) {
-  if (bills<200) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-console.log(budget(199));
-console.log("After I pay this off I can still go shopping.");
